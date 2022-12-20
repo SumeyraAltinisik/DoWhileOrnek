997b3aa7f5f87dc0b52b6a501e2f588e0d873d41
SumeyraAltinisik <sfa2023@gmail.com>
2022-12-20 18:59:46 +0300

    Add project files.

---
+++ DoWhileOrnek/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = new Random().Next(0, 100);
+            int tahmin = 0;
+            do
+            {
+                Console.WriteLine("Tuttuğum sayıyı tahmin et.");
+                tahmin = int.Parse(Console.ReadLine());
+                if (tahmin > sayi)
+                {
+                    Console.WriteLine("Aşağı in");
+                }
+                else if (tahmin < sayi)
+                    Console.WriteLine("Yukarı çık");
+            } while (tahmin != sayi);
+            Console.WriteLine("Tebrikler!");
+            Console.ReadKey();
+        }
+    }
+}
